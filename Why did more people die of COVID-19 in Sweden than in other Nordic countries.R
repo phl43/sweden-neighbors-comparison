9a4bab69bb49cd43ba40efe68dff1e7ede46379a
Philippe Lemoine <36411538+phl43@users.noreply.github.com>
2020-12-26 20:36:45 +0100

    Fixed counterfactual simulation + added parallel processing

--- Why did more people die of COVID-19 in Sweden than in other Nordic countries.R
+++ Why did more people die of COVID-19 in Sweden than in other Nordic countries.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(lubridate)
+library(furrr)
+library(progressr)
 library(cowplot)
 library(zoo)
 
@@ -9,7 +11,7 @@ infer_infections_draw <- function(
   ifr = 0.01,
   mu_i2d = 3.19,
   sigma_i2d = 0.44
-  ) {
+) {
   data_length <- length(deaths)
   
   infections <- rep(0, data_length)
@@ -38,8 +40,9 @@ infer_infections <- function(
   n_sims,
   ifr,
   mu_i2d,
-  sigma_i2d
-  ) {
+  sigma_i2d,
+  rng_seed
+) {
   draws <- list()
   
   for (i in 1:length(countries)) {
@@ -49,17 +52,33 @@ infer_infections <- function(
       filter(country == c) %>%
       arrange(date)
     
-    for (j in 1:n_sims) {
-      draws[[c]][[j]] <- tibble(
-        date = country_data$date,
-        country = c,
-        infections = infer_infections_draw(country_data$deaths, ifr[i], mu_i2d[i], sigma_i2d[i])
+    print(paste0("Estimation of infections for ", c, " starts"))
+    
+    args <- list(
+      deaths = rep(list(country_data$deaths), n_sims),
+      ifr = rep(list(ifr[i]), n_sims),
+      mu_i2d = rep(list(mu_i2d[i]), n_sims),
+      sigma_i2d = rep(list(sigma_i2d[i]), n_sims)
       )
-    }
     
-    draws[[c]] <- bind_rows(draws[[c]])
+    with_progress({
+      p <- progressor(steps = n_sims)
+      
+      draws[[c]] <- future_pmap_dfr(
+        args,
+        function(deaths, ifr, mu_i2d, sigma_i2d) {
+          p()
+          tibble(
+            date = country_data$date,
+            country = c,
+            infections = infer_infections_draw(deaths, ifr, mu_i2d, sigma_i2d)
+          )
+        },
+        .options = furrr_options(seed = rng_seed)
+      )
+    })
     
-    print(paste0("Estimation for ", c, " completed"))
+    print(paste0("Estimation of infections for ", c, " completed"))
   }
   
   results <- bind_rows(draws) %>%
@@ -85,7 +104,7 @@ simulate_counterfactual_epidemic <- function(
   ifr = 0.01,
   mu_i2d = 3.19,
   sigma_i2d = 0.44
-  ) {
+) {
   # parameters of the gamma distribution I use to model the generation time distribution
   gt_alpha <- mean_gt^2 / sd_gt^2
   gt_beta <- mean_gt / sd_gt^2
@@ -142,7 +161,8 @@ simulate_infections_with_counterfactual <- function(
   sd_gt,
   ifr,
   mu_i2d,
-  sigma_i2d
+  sigma_i2d,
+  rng_seed
 ) {
   draws <- list()
   
@@ -155,38 +175,77 @@ simulate_infections_with_counterfactual <- function(
     
     lockdown_start <- as.integer(lockdowns$date[lockdowns$country == c] - country_data$date[1] + 1)
     
-    for (j in 1:n_sims) {
-      # I only use deaths that occurred up to 60 days after the start of the lockdown in this country since
-      # I will only use infections that occurred before that date for the counterfactual and presumably no one
-      # or almost no one who died more than 60 days later should have been infected before that, so this will
-      # avoid unnecessary computations
-      infections_actual <- infer_infections_draw(
-        country_data$deaths[1:(lockdown_start + 60)],
-        ifr[i],
-        mu_i2d[i],
-        sigma_i2d[i]
-        )
+    print(paste0("Estimation of infections for ", c, " starts"))
+    
+    # I only use deaths that occurred up to 60 days after the start of the lockdown in this country since I will only
+    # use infections that occurred before that date for the counterfactual and presumably no one or almost no one who
+    # died more than 60 days later should have been infected before that, so this will avoid unnecessary computations
+    args1 <- list(
+      deaths = rep(list(country_data$deaths[1:(lockdown_start + 60)]), n_sims),
+      ifr = rep(list(ifr[i]), n_sims),
+      mu_i2d = rep(list(mu_i2d[i]), n_sims),
+      sigma_i2d = rep(list(sigma_i2d[i]), n_sims)
+      )
+    
+    with_progress({
+      p <- progressor(steps = n_sims)
       
-      lockdown_start <- lockdowns$date[lockdowns$country == c] - country_data$date[1]
-      draws[[c]][[j]] <- simulate_counterfactual_epidemic(
-        infections_actual[1:lockdown_start],
-        R_after_lockdown,
-        simulation_length,
-        mean_gt[i],
-        sd_gt[i],
-        ifr[i],
-        mu_i2d[i],
-        sigma_i2d[i]
-      ) %>%
-        add_column(
-          date = country_data$date[1:(lockdown_start + simulation_length)],
-          country = c
-        )
-    }
+      infections_actual <- future_pmap(
+        args1,
+        function(deaths, ifr, mu_i2d, sigma_i2d) {
+          p()
+          infer_infections_draw(deaths, ifr, mu_i2d, sigma_i2d)
+        },
+        .options = furrr_options(seed = rng_seed)
+      )
+    })
+    
+    print(paste0("Estimation of infections for ", c, " completed"))
+    
+    print(paste0("Estimation of counterfactual for ", c, " starts"))
+    
+    infections_actual <- map(infections_actual, ~ .x[1:lockdown_start])
+    
+    args2 <- list(
+      infections_before_lockdown = infections_actual,
+      R_after_lockdown = rep(list(R_after_lockdown), n_sims),
+      simulation_length = rep(list(simulation_length), n_sims),
+      mean_gt = rep(list(mean_gt[i]), n_sims),
+      sd_gt = rep(list(sd_gt[i]), n_sims),
+      ifr = rep(list(ifr[i]), n_sims),
+      mu_i2d = rep(list(mu_i2d[i]), n_sims),
+      sigma_i2d = rep(list(sigma_i2d[i]), n_sims)
+      )
+    
+    with_progress({
+      p <- progressor(steps = n_sims)
+      
+      draws[[c]] <- future_pmap_dfr(
+        args2,
+        function(infections_before_lockdown, R_after_lockdown, simulation_length, mean_gt, sd_gt, ifr, mu_i2d, sigma_i2d) {
+          p()
+          simulate_counterfactual_epidemic(
+            infections_before_lockdown,
+            R_after_lockdown,
+            simulation_length,
+            mean_gt,
+            sd_gt,
+            ifr,
+            mu_i2d,
+            sigma_i2d
+          ) %>%
+            add_column(
+              date = country_data$date[1:(lockdown_start + simulation_length)],
+              country = c
+            )
+        },
+        .options = furrr_options(seed = rng_seed)
+      )
+    })
     
     draws[[c]] <- bind_rows(draws[[c]])
     
-    print(paste0("Estimation for ", c, " completed"))
+    print(paste0("Estimation of counterfactual for ", c, " completed"))
   }
   
   results <- bind_rows(draws) %>%
@@ -208,15 +267,19 @@ simulate_infections_with_counterfactual <- function(
     ungroup()
 }
 
+# allow parallel processing
+plan(multisession)
+
 # seed for reproducibility
-set.seed(127)
+# (see https://www.r-bloggers.com/2020/09/future-1-19-1-making-sure-proper-random-numbers-are-produced-in-parallel-processing/ and https://davisvaughan.github.io/furrr/reference/furrr_options.html#reproducible-random-number-generation-rng-)
+rng_seed <- 127
 
 owid_data_url <- "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"
 
 owid_data <- read_csv(owid_data_url) %>%
   rename(
     country = location
-    ) %>%
+  ) %>%
   mutate(
     deaths = replace_na(new_deaths, 0),
     rolling_average_deaths = round(rollmean(deaths, 7, fill = c(0, 0, 0), align = "right"))
@@ -226,7 +289,7 @@ owid_data <- read_csv(owid_data_url) %>%
     country,
     deaths,
     rolling_average_deaths
-    )
+  )
 
 # sometimes the number of deaths for a day is negative because there was a data correction, but it's almost never the
 # case for Nordic countries and, even when it happens, the corrections are tiny, so I just set the number of deaths on
@@ -262,8 +325,9 @@ estimates_nordic <- infer_infections(
   1e5,
   ifr = c(0.01, 0.01, 0.01, 0.01),
   mu_i2d = c(3.19, 3.19, 3.19, 3.19),
-  sigma_i2d = c(0.44, 0.44, 0.44, 0.44)
-  )
+  sigma_i2d = c(0.44, 0.44, 0.44, 0.44),
+  rng_seed = rng_seed
+)
 
 data_for_plot_nordic <- estimates_nordic %>%
   filter(date >= ymd("2020-02-01") & date <= ymd("2020-06-01"))
@@ -322,7 +386,8 @@ counterfactual_estimates <- simulate_infections_with_counterfactual(
   sd_gt = c(2.96, 2.96, 2.96, 2.96),
   ifr = c(0.01, 0.01, 0.01, 0.01),
   mu_i2d = c(3.19, 3.19, 3.19, 3.19),
-  sigma_i2d = c(0.44, 0.44, 0.44, 0.44)
+  sigma_i2d = c(0.44, 0.44, 0.44, 0.44),
+  rng_seed = rng_seed
 )
 
 data_for_plot_counterfactual <- counterfactual_estimates %>%
@@ -408,7 +473,8 @@ counterfactual_estimates_sensitivity <- simulate_infections_with_counterfactual(
   sd_gt = c(2.96, 2.96, 2.96, 2.96),
   ifr = c(0.005, 0.005, 0.005, 0.01),
   mu_i2d = c(3.19, 3.19, 3.19, 2.95),
-  sigma_i2d = c(0.44, 0.44, 0.44, 0.44)
+  sigma_i2d = c(0.44, 0.44, 0.44, 0.44),
+  rng_seed = rng_seed
 )
 
 data_for_plot_counterfactual_sensitivity <- counterfactual_estimates_sensitivity %>%
@@ -419,7 +485,7 @@ counterfactual_infections_plot_sensitivity <- ggplot(data_for_plot_counterfactua
   geom_ribbon(aes(ymin = infections_lower1, ymax = infections_upper1), linetype = 0, alpha = 0.1, show.legend = FALSE) +
   geom_ribbon(aes(ymin = infections_lower2, ymax = infections_upper2), linetype = 0, alpha = 0.1, show.legend = FALSE) +
   theme_bw() +
-  ggtitle("Daily number of COVID-19 infections in Nordic countries in counterfactual where\nR was suddenly reduced to 0.6 everywhere on March 15 (sensitivity analysis)") +
+  ggtitle("Daily number of COVID-19 infections in Nordic countries in counterfactual where\nR was suddenly reduced to 0.6 everywhere on March 15 (IFR = 0.5% in Denmark/Finland/Norway and 1% in Sweden)") +
   xlab("Date") +
   ylab("Daily number of COVID-19 infections") +
   scale_color_discrete(name = "Country") +
@@ -438,7 +504,7 @@ counterfactual_deaths_plot_sensitivity <- ggplot(data_for_plot_counterfactual_se
   geom_ribbon(aes(ymin = deaths_lower1, ymax = deaths_upper1), linetype = 0, alpha = 0.1, show.legend = FALSE) +
   geom_ribbon(aes(ymin = deaths_lower2, ymax = deaths_upper2), linetype = 0, alpha = 0.1, show.legend = FALSE) +
   theme_bw() +
-  ggtitle("Daily number of COVID-19 deaths in Nordic countries in counterfactual where\nR was suddenly reduced to 0.6 everywhere on March 15 (sensitivity analysis)") +
+  ggtitle("Daily number of COVID-19 deaths in Nordic countries in counterfactual where\nR was suddenly reduced to 0.6 everywhere on March 15 (IFR of 0.5% in Denmark/Finland/Norway and 1% in Sweden)") +
   xlab("Date") +
   ylab("Daily number of COVID-19 deaths") +
   scale_color_discrete(name = "Country") +
@@ -459,7 +525,75 @@ plot_grid(
   ncol = 1
 ) +
   ggsave(
-    "Daily number of COVID-19 infections and deaths in Nordic countries in counterfactual where R was suddenly reduced to 0.6 everywhere on March 15 (sensitivity analysis).png",
+    "Daily number of COVID-19 infections and deaths in Nordic countries in counterfactual where R was suddenly reduced to 0.6 everywhere on March 15 (first sensitivity analysis).png",
+    width = 12,
+    height = 12
+  )
+
+counterfactual_estimates_sensitivity2 <- simulate_infections_with_counterfactual(
+  owid_data,
+  nordic_countries,
+  1e5,
+  counterfactual_lockdowns,
+  0.6,
+  120,
+  mean_gt = c(3.99, 3.99, 3.99, 3.99),
+  sd_gt = c(2.96, 2.96, 2.96, 2.96),
+  ifr = c(0.005, 0.002, 0.002, 0.01),
+  mu_i2d = c(3.19, 3.19, 3.19, 2.95),
+  sigma_i2d = c(0.44, 0.44, 0.44, 0.44),
+  rng_seed = rng_seed
+)
+
+data_for_plot_counterfactual_sensitivity2 <- counterfactual_estimates_sensitivity2 %>%
+  filter(date >= ymd("2020-02-01") & date <= ymd("2020-07-01"))
+
+counterfactual_infections_plot_sensitivity2 <- ggplot(data_for_plot_counterfactual_sensitivity2, aes(x = date, y = infections_median, group = country, color = country)) +
+  geom_line(size = 1) +
+  geom_ribbon(aes(ymin = infections_lower1, ymax = infections_upper1), linetype = 0, alpha = 0.1, show.legend = FALSE) +
+  geom_ribbon(aes(ymin = infections_lower2, ymax = infections_upper2), linetype = 0, alpha = 0.1, show.legend = FALSE) +
+  theme_bw() +
+  ggtitle("Daily number of COVID-19 infections in Nordic countries in counterfactual where\nR was suddenly reduced to 0.6 everywhere on March 15 (IFR of 0.5% in Denmark, 0.2% in Finland/Norway and 1% in Sweden)") +
+  xlab("Date") +
+  ylab("Daily number of COVID-19 infections") +
+  scale_color_discrete(name = "Country") +
+  scale_x_date(
+    labels = scales::date_format("%m/%d/%Y"),
+    date_breaks = "7 day"
+  ) +
+  labs(caption = "Source of data: Our World in Data - Chart by Philippe Lemoine (@phl43)") +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    axis.text.x = element_text(angle = 90, vjust = 0.5)
+  )
+
+counterfactual_deaths_plot_sensitivity2 <- ggplot(data_for_plot_counterfactual_sensitivity2, aes(x = date, y = deaths_median, group = country, color = country)) +
+  geom_line(size = 1) +
+  geom_ribbon(aes(ymin = deaths_lower1, ymax = deaths_upper1), linetype = 0, alpha = 0.1, show.legend = FALSE) +
+  geom_ribbon(aes(ymin = deaths_lower2, ymax = deaths_upper2), linetype = 0, alpha = 0.1, show.legend = FALSE) +
+  theme_bw() +
+  ggtitle("Daily number of COVID-19 deaths in Nordic countries in counterfactual where\nR was suddenly reduced to 0.6 everywhere on March 15 (IFR of 0.5% in Denmark, 0.2% in Finland/Norway and 1% in Sweden)") +
+  xlab("Date") +
+  ylab("Daily number of COVID-19 deaths") +
+  scale_color_discrete(name = "Country") +
+  scale_x_date(
+    labels = scales::date_format("%m/%d/%Y"),
+    date_breaks = "7 day"
+  ) +
+  labs(caption = "Source of data: Our World in Data - Chart by Philippe Lemoine (@phl43)") +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    axis.text.x = element_text(angle = 90, vjust = 0.5)
+  )
+
+plot_grid(
+  counterfactual_infections_plot_sensitivity2,
+  counterfactual_deaths_plot_sensitivity2,
+  labels = c("", ""),
+  ncol = 1
+) +
+  ggsave(
+    "Daily number of COVID-19 infections and deaths in Nordic countries in counterfactual where R was suddenly reduced to 0.6 everywhere on March 15 (second sensitivity analysis).png",
     width = 12,
     height = 12
   )
